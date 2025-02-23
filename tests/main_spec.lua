fa973fd5c0c649b5bbe65404b2f65fc96300d5ca
Aaron Crosman <acrosman@gmail.com>
2025-02-23 16:02:18 -0500

    Add setup and tear down

--- tests/main_spec.lua
+++ tests/main_spec.lua
@@ -26,9 +26,26 @@ local function mock_http_request(params)
 end
 
 describe("main.lua tests", function()
+    local original_popen
+    local original_http_request
+
+    -- Setup mocks before each test
+    before_each(function()
+        original_popen = io.popen
+        io.popen = mock_popen
+
+        original_http_request = require("socket.http").request
+        package.loaded["socket.http"].request = mock_http_request
+    end)
+
+    -- Restore original functions after each test
+    after_each(function()
+        io.popen = original_popen
+        package.loaded["socket.http"].request = original_http_request
+    end)
+
     -- Test get_interface_status function
     it("should get the status of a network interface", function()
-        _G.io.popen = mock_popen
         local status = get_interface_status("wan")
         assert.are.same(status.up, true)
         assert.are.same(status.uptime, 12345)
@@ -36,7 +53,6 @@ describe("main.lua tests", function()
 
     -- Test get_wifi_status function
     it("should get the status of wifi radios", function()
-        _G.io.popen = mock_popen
         local status = get_wifi_status()
         assert.are.same(status.radio0.up, true)
         assert.are.same(status.radio0.uptime, 12345)
@@ -44,10 +60,9 @@ describe("main.lua tests", function()
 
     -- Test send_post_request function
     it("should send a POST request and receive a response", function()
-        _G.http.request = mock_http_request
         local response, code, headers, status = send_post_request("https://spinningcode.org/api/endpoint",
             '{"test": "data"}')
-        assert.are.same(response, "OK")
-        assert.are.same(code, 200)
+        assert.are.same("OK", response)
+        assert.are.same(200, code)
     end)
 end)
