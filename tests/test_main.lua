8cb5cead8bed4c3972a223cb54adadda5fd2b8be
Aaron Crosman <acrosman@gmail.com>
2025-02-23 14:26:22 -0500

    Rewrite tests to busted

--- tests/test_main.lua
+++ tests/test_main.lua
@@ -1,5 +1,5 @@
-local luatest = require('luatest')
-local g = luatest.group('main_tests')
+local busted = require('busted')
+local json = require('dkjson')
 
 -- Mocking io.popen to simulate ubus call responses
 local function mock_popen(command)
@@ -22,26 +22,29 @@ local function mock_http_request(params)
     return "OK", 200, {}, "HTTP/1.1 200 OK"
 end
 
--- Test get_interface_status function
-g.test_get_interface_status = function()
-    _G.io.popen = mock_popen
-    local status = get_interface_status("wan")
-    luatest.assert_equals(status.up, true)
-    luatest.assert_equals(status.uptime, 12345)
-end
+describe("main.lua tests", function()
+    -- Test get_interface_status function
+    it("should get the status of a network interface", function()
+        _G.io.popen = mock_popen
+        local status = get_interface_status("wan")
+        assert.are.same(status.up, true)
+        assert.are.same(status.uptime, 12345)
+    end)
 
--- Test get_wifi_status function
-g.test_get_wifi_status = function()
-    _G.io.popen = mock_popen
-    local status = get_wifi_status()
-    luatest.assert_equals(status.radio0.up, true)
-    luatest.assert_equals(status.radio0.uptime, 12345)
-end
+    -- Test get_wifi_status function
+    it("should get the status of wifi radios", function()
+        _G.io.popen = mock_popen
+        local status = get_wifi_status()
+        assert.are.same(status.radio0.up, true)
+        assert.are.same(status.radio0.uptime, 12345)
+    end)
 
--- Test send_post_request function
-g.test_send_post_request = function()
-    _G.http.request = mock_http_request
-    local response, code, headers, status = send_post_request("https://spinningcode.org/api/endpoint", '{"test": "data"}')
-    luatest.assert_equals(response, "OK")
-    luatest.assert_equals(code, 200)
-end
+    -- Test send_post_request function
+    it("should send a POST request and receive a response", function()
+        _G.http.request = mock_http_request
+        local response, code, headers, status = send_post_request("https://spinningcode.org/api/endpoint",
+            '{"test": "data"}')
+        assert.are.same(response, "OK")
+        assert.are.same(code, 200)
+    end)
+end)
